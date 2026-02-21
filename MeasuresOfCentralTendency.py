a9eee84d7474f4ab0aac31441d2e36e89c9c18a0
tiwari-shilpa-usv <s016561@usventure.com>
2026-02-21 13:43:51 -0600

    code changes

---
+++ MeasuresOfCentralTendency.py
@@ -0,0 +1,9 @@
+import scipy;
+import pandas as pd;
+from sklearn.datasets import load_iris;
+
+d = pd.DataFrame(load_iris().data, columns=load_iris().feature_names)
+print("DataFrame:", d)
+print("Mean:", d.mean())
+print("Median:", d.median())
+print("Mode:", d.mode())
